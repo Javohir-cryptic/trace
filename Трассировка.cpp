5790acf331d67ee1bb85901053e31bce2da651b0
Javohir-cryptic <javohirerkinovich@gmail.com>
2020-08-14 20:10:58 +0300

    Первый коммит

---
+++ Трассировка.cpp
@@ -0,0 +1,21 @@
+﻿// Трассировка.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#pragma warning(disable : 4996)
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int i, X;
+	printf("Введите целое число:\n");
+	scanf("%d", &X);
+	printf("Введено X=%d\n", X);              // точка 1  
+	for (i = 1; i < 10; i++)
+	{
+		printf("В цикле: i=%d, X=%d\n", i, X);  // точка 2 
+		
+	}
+	printf("После цикла: X=%d\n", X);         // точка 3 
+	
+}
